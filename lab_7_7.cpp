e671748a96dc69204413658104e2a2eee356ffce
Unknown <koynanthiya369@gmail.com>
2018-01-22 18:20:05 +0700

    แก้ข้อ7

--- lab_7_7.cpp
+++ lab_7_7.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main () {
 
   // เขียนโปรแกรมเพื่อรับค่าตัวเลข 5 จำนวน และหาค่าเฉลี่ย
-
+    int n;
+    int sum = 0;
+    float avg = 0;
+    for(int i=0; i < 5; i++)
+    {
+        cin >> n;
+        sum += n;
+    }
+    avg = sum / 5;
+    cout << avg<< endl;
   return 0;
 }
